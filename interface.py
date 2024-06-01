cc0a270e292291b9cf20e4c415ce909224b55a72
smrutipote <129295962+smrutipote@users.noreply.github.com>
2024-06-01 19:58:05 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -17,39 +17,23 @@ def obese_model():
 ########################################## Model API ##############################################################
 ###################################################################################################################
 
-@app.route('/predict_obesity', methods = ['POST','GET'])
+@app.route('/predict_obesity', methods = ["POST"])
 def get_obesity():
-    if request.method == 'POST':
-        print('We are in POST Method')
-        data = request.form
-        age = eval(data['age'])
-        gender = data['gender']
-        height = eval(data['height'])
-        weight =  eval(data['weight'])
-        bmi = eval(data['bmi'])
-        
-        mk_pred = MakePrediction(age, gender, height, weight, bmi)
-        final = mk_pred.get_prediction()
-        if final == 0:
-            return jsonify({'Result': f'Test for obesity is Negative'})
-        else:
-            return jsonify({'Result': f'Test for obesity is Positive'})
-
+    print('We are in POST Method')
+    data = request.form
+    age = eval(data['age'])
+    gender = data['gender']
+    height = eval(data['height'])
+    weight =  eval(data['weight'])
+    bmi = eval(data['bmi'])
+    
+    mk_pred = MakePrediction(age, gender, height, weight, bmi)
+    final = mk_pred.get_prediction()
+    if final == 0:
+        return jsonify({'Result': f'Test for obesity is Negative'})
     else:
-        print('We are in GET Method')
-        data1 = request.args
-        age = eval(data1.get('age'))
-        gender = data1.get('gender')
-        height = eval(data1.get('height'))
-        weight = eval(data1.get('weight'))
-        bmi = eval(data1.get('bmi'))
-
-        mk_pred1 = MakePrediction(age, gender, height, weight, bmi)
-        final1 = mk_pred1.get_prediction()
-        if final1 == 0:
-            return jsonify({'Result': f'Test for obesity is Negative'})
-        else:
-            return jsonify({'Result': f'Test for obesity is Positive'})
+        return jsonify({'Result': f'Test for obesity is Positive'})
         
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = config.PORT_NUMBER, debug=False)
+    
